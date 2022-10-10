dbd9cd24de18ac19b41d72f192b9c4911f6433d1
Kristiyan Strahilov <55084258+kpuc00@users.noreply.github.com>
2022-10-10 14:26:04 +0200

    StopManager WIP

--- app/src/main/java/dev/kstrahilov/tnavi/CreateStop.kt
+++ app/src/main/java/dev/kstrahilov/tnavi/CreateStop.kt
@@ -4,44 +4,65 @@ import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.EditText
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.*
+import com.google.gson.Gson
+import java.io.File
 
 
 class CreateStop : AppCompatActivity() {
     private lateinit var etStopTitle: EditText
     private lateinit var selectedLocation: LatLng
+    private var stop: Stop? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_stop)
         title = application.getString(R.string.label_create_stop)
+        val intent = intent
+        try {
+            stop = (intent.getParcelableExtra("stop") as Stop?)!!
+        } catch (_: java.lang.NullPointerException) {
+        }
 
         etStopTitle = findViewById(R.id.et_stop_title)
 
-        val testStop = Stop(title = "8-ми декември", location = LatLng(43.203468, 27.900332))
+        if (stop != null) {
+            etStopTitle.setText(stop!!.title)
+            etStopTitle.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
 
-        etStopTitle.setText(testStop.title)
+                override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+                override fun afterTextChanged(p0: Editable?) {
+                    stop!!.title = etStopTitle.text.toString()
+                }
+
+            })
+        }
 
         val mapFragment = supportFragmentManager.findFragmentById(
             R.id.map
         ) as? SupportMapFragment
         mapFragment?.getMapAsync { googleMap ->
-            googleMap.addMarker(
-                MarkerOptions().position(testStop.location)
-                    .icon(bitmapFromVector(applicationContext, R.drawable.spirka))
-                    .anchor(0.5F, 0.5F)
-            )
-            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(testStop.location, 17F))
-            addCircle(googleMap, testStop.location)
+            if (stop != null) {
+                googleMap.addMarker(
+                    MarkerOptions().position(stop!!.location)
+                        .icon(bitmapFromVector(applicationContext, R.drawable.spirka))
+                        .anchor(0.5F, 0.5F)
+                )
+                googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(stop!!.location, 17F))
+                addCircle(googleMap, stop!!.location)
+            }
 
             googleMap.setOnMapClickListener {
                 selectedLocation = it
@@ -52,7 +73,9 @@ class CreateStop : AppCompatActivity() {
                         .anchor(0.5F, 0.5F)
                 )
                 addCircle(googleMap, selectedLocation)
-                testStop.location = it
+                if (stop != null) {
+                    stop!!.location = it
+                }
             }
         }
     }
@@ -64,11 +87,32 @@ class CreateStop : AppCompatActivity() {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean = when (item.itemId) {
         R.id.action_save -> {
-            Toast.makeText(applicationContext, "save button", Toast.LENGTH_SHORT).show()
+            saveStop()
 
             true
         }
         else -> super.onOptionsItemSelected(item)
+    }
+
+    private fun saveStop() {
+        val stops: ArrayList<Stop> = ArrayList()
+        if (stop != null) {
+            stops.add(stop!!)
+        } else {
+            stops.add(Stop(title = etStopTitle.text.toString().trim(), location = selectedLocation))
+        }
+
+        val path = "stops.json"
+        try {
+            File(path).createNewFile()
+            File(path).printWriter().use {
+                val gson = Gson()
+                val jsonString = gson.toJson(stops)
+                it.write(jsonString)
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
 
     private var circle: Circle? = null
@@ -76,25 +120,29 @@ class CreateStop : AppCompatActivity() {
     private fun addCircle(googleMap: GoogleMap, latLng: LatLng) {
         circle?.remove()
         circle = googleMap.addCircle(
-            CircleOptions()
-                .center(latLng)
-                .radius(50.0)
+            CircleOptions().center(latLng).radius(50.0)
                 .strokeColor(ContextCompat.getColor(this, R.color.blue))
         )
     }
 
+//    fun addObject(path: String, name: String, value: String) {
+//        val gson = Gson()
+//        val reader = FileReader(File(path))
+//        val type = object : TypeToken<Map<String, String>>() {}.type
+//        println("Type: $type")
+//        val existingJson = gson.fromJson<Map<String, String>>(JsonReader(reader), type)
+//        println("Existing Json: $existingJson")
+//        val newJsonMap = existingJson.plus(Pair(name, value))
+//        FileWriter(File(path)).use { writer -> writer.write(gson.toJson(newJsonMap)) }
+//    }
+
     private fun bitmapFromVector(context: Context, vectorResId: Int): BitmapDescriptor {
         val vectorDrawable = ContextCompat.getDrawable(context, vectorResId)
         vectorDrawable!!.setBounds(
-            0,
-            0,
-            vectorDrawable.intrinsicWidth,
-            vectorDrawable.intrinsicHeight
+            0, 0, vectorDrawable.intrinsicWidth, vectorDrawable.intrinsicHeight
         )
         val bitmap = Bitmap.createBitmap(
-            vectorDrawable.intrinsicWidth,
-            vectorDrawable.intrinsicHeight,
-            Bitmap.Config.ARGB_8888
+            vectorDrawable.intrinsicWidth, vectorDrawable.intrinsicHeight, Bitmap.Config.ARGB_8888
         )
         val canvas = Canvas(bitmap)
         vectorDrawable.draw(canvas)
--- app/src/main/java/dev/kstrahilov/tnavi/StopManager.kt
+++ app/src/main/java/dev/kstrahilov/tnavi/StopManager.kt
@@ -4,13 +4,61 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.model.LatLng
 
-class StopManager : AppCompatActivity() {
+class StopManager : AppCompatActivity(), AdapterView.OnItemClickListener {
+    private lateinit var lvAllStops: ListView
+    private lateinit var stops: ArrayList<Stop>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_stop_manager)
         title = application.getString(R.string.stops_manager)
+
+        lvAllStops = findViewById(R.id.lv_all_stops)
+
+        stops = ArrayList()
+        stops.add(Stop(title = "Бл. 407 (Вл-во)"))
+        stops.add(Stop(title = "кап. Петко войвода 2"))
+        stops.add(Stop(title = "Детелина"))
+        stops.add(Stop(title = "Вежен"))
+        stops.add(Stop(title = "Мургаш"))
+        stops.add(Stop(title = "Армейска"))
+        stops.add(Stop(title = "ТИС Север"))
+        stops.add(Stop(title = "Осъм"))
+        stops.add(Stop(title = "3-ти март"))
+        stops.add(Stop(title = "Искър"))
+        stops.add(Stop(title = "Янтра"))
+        stops.add(Stop(title = "Огоста"))
+        stops.add(Stop(title = "Централна автобаза"))
+        stops.add(Stop(title = "Дом Младост (за Аспарухово)"))
+        stops.add(Stop(title = "8-ми септември"))
+        stops.add(Stop(title = "Техникумите-Сливница"))
+        stops.add(Stop(title = "Трансформатора"))
+        stops.add(Stop(title = "Л.к. Тракия"))
+        stops.add(Stop(title = "Нептун"))
+        stops.add(
+            Stop(
+                title = "Централна поща (за Аспарухово)", location = LatLng(51.4652, 5.452)
+            )
+        )
+        stops.add(Stop(title = "Полиграфия"))
+        stops.add(Stop(title = "8-ми декември"))
+        stops.add(Stop(title = "КЗ"))
+        stops.add(Stop(title = "ИХА"))
+        stops.add(Stop(title = "Лазур"))
+        stops.add(Stop(title = "Калин"))
+        stops.add(Stop(title = "Любен Каравелов"))
+        stops.add(Stop(title = "Народни Будители"))
+        stops.add(Stop(title = "Обръщач тролеи"))
+
+        lvAllStops.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, stops)
+        lvAllStops.onItemClickListener = this
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -26,5 +74,12 @@ class StopManager : AppCompatActivity() {
             true
         }
         else -> super.onOptionsItemSelected(item)
+    }
+
+    override fun onItemClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        val stop: Stop = stops[position]
+        val intent = Intent(this, CreateStop::class.java)
+        intent.putExtra("stop", stop)
+        startActivity(intent)
     }
 }
